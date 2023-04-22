4aac8784e9ac179c86b013e258dc78f56d69d94e
samarthsoni3002 <samarthsoni3002@gmail.com>
2023-04-22 11:31:02 +0530

    Background image added

--- front_end.py
+++ front_end.py
@@ -1,8 +1,22 @@
 from tkinter import *
 import customtkinter
+from PIL import ImageTk, Image
+
+customtkinter.set_default_color_theme("blue")
 
 app = customtkinter.CTk()
-app.geometry("2000x2000")
+app.geometry("1000X1000")
 app.title("Time Capsule Object Viewer")
+
+app.resizable(False,False)
+
+img = Image.open('./bgimage3.jpg')
+im = img.resize((1000,1000))
+
+
+bg_image = ImageTk.PhotoImage(im)
+bg = Canvas(app, width=1000, height=1000)
+bg.pack(fill=BOTH, expand=YES)
+bg.create_image(0,0,image=bg_image,anchor=NW)
 
 app.mainloop()
