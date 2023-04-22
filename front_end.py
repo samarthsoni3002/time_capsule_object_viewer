9fe903b01c9b6c57844c94ae0c34f8baec0a50eb
Khushig11 <khushig1102@gmail.com>
2023-04-22 11:35:54 +0530

    tabs added

--- front_end.py
+++ front_end.py
@@ -5,4 +5,15 @@ app = customtkinter.CTk()
 app.geometry("2000x2000")
 app.title("Time Capsule Object Viewer")
 
+tabview = customtkinter.CTkTabview(app)
+tabview.pack(padx=100, pady=100)
+
+tabview.add("HOME")  
+tabview.add("RELIVE THE PAST")  
+tabview.add("DIVE INTO THE FUTURE") 
+tabview.set("HOME")  
+
+
+
+
 app.mainloop()
