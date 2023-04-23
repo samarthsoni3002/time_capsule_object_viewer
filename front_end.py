a6b334b11eee07f1d2e19bb552313bf719e4acba
samarthsoni3002 <samarthsoni3002@gmail.com>
2023-04-23 08:27:32 +0530

    Final frontend added

--- front_end.py
+++ front_end.py
@@ -2,41 +2,20 @@ from tkinter import *
 import customtkinter
 from PIL import ImageTk, Image
 
-customtkinter.set_default_color_theme("blue")
+
 
 app = customtkinter.CTk()
 app.geometry("1000X1000")
 app.title("Time Capsule Object Viewer")
 
 
-def button_event():
-    print("button pressed")
 
-button1 = customtkinter.CTkButton(master=app,  
-                                 width=400,
-                                 height=100,
-                                 border_width=0,
-                                 corner_radius=8,
-                                 text="HOME",
-                                 command=button_event) 
-button1.grid(row=0, column=0, padx=10, pady=350)
-button2 = customtkinter.CTkButton(master=app,  
-                                 width=400,
-                                 height=100,
-                                 border_width=0,
-                                 corner_radius=8,
-                                 text="RELIVE THE PAST",
-                                 command=button_event) 
+frame = Frame(app,width=600,height=400)
+frame.pack()
+frame.place(anchor='center',relx=0.5,rely=0.5)
 
-button2.grid(row=0, column=1, padx=20, pady=350)
-button3 = customtkinter.CTkButton(master=app,  
-                                 width=400,
-                                 height=100,
-                                 border_width=0,
-                                 corner_radius=8,
-                                 text="DIVE INTO THE FUTURE",
-                                 command=button_event) 
-
-button3.grid(row=0, column=2, padx=20, pady=350)
+bg = ImageTk.PhotoImage(Image.open("./TIME CAPSULE.jpg"))
+label = Label(frame,image=bg)
+label.pack()
 
 app.mainloop()
